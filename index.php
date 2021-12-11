1ac2502575a8bfb05af6db2c6ae07b9bb0e3386c
Dileepa_Malshan <84204637+Dileepa-Malshan@users.noreply.github.com>
2021-12-11 12:46:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 ///https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
-$botToken = ""; // Enter ur bot token
+$botToken = "2137032424:AAGiQ80Qs79BIq-YSbxfEyz4MyuWOdv_FIE"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
